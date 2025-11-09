e9d7db2ed3bb29b3d41b3ea01d86bc7810f5d196
iam-siva25 <sivabalasubramanian2502@gmail.com>
2025-11-09 17:47:51 +0530

    CI/CD: build & FTP deploy (CI=false, correct slashes, version marker)

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './Theme'; // Import your custom theme
 import { HelmetProvider } from 'react-helmet-async';
 
-// Check if you have a service worker file and unregister it to prevent caching issues
-// If you don't have this file, you can remove this line and the unregister call below
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
+// REMOVED: import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
+// REMOVED: serviceWorkerRegistration.unregister(); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,6 +18,3 @@ root.render(
     </HelmetProvider>
   </React.StrictMode>
 );
-
-// Call unregister() instead of register() to stop aggressive caching
-serviceWorkerRegistration.unregister();
