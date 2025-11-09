3acbc83779b5dd3bdebced2b644145790b785e0e
iam-siva25 <sivabalasubramanian2502@gmail.com>
2025-11-09 23:07:36 +0530

    final deploy "

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './Theme'; // Import your custom theme
-import { HelmetProvider } from 'react-helmet-async';
-
-// REMOVED: import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
-// REMOVED: serviceWorkerRegistration.unregister(); 
+import { HelmetProvider } from 'react-helmet-async'; // <-- Add this
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
+    <HelmetProvider>             {/* <-- Wrap your app in HelmetProvider */}
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
